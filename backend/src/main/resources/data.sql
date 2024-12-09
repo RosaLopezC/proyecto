95b08bdf51bb2e5acad7b9247fb9d9aab7f52d41
aaronnole <aaron.nole@tecsup.edu.pe>
2024-12-09 15:26:12 -0500

    sentencias sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -29,3 +29,112 @@ INSERT INTO grado (nivel_academico_id, nombre_grado)
 SELECT id, 'Cuarto' FROM nivel_academico WHERE nombre = 'Secundaria';
 INSERT INTO grado (nivel_academico_id, nombre_grado)
 SELECT id, 'Quinto' FROM nivel_academico WHERE nombre = 'Secundaria';
+INSERT INTO curso (titulo) VALUES ('Números');
+INSERT INTO curso (titulo) VALUES ('Matemáticas');
+INSERT INTO curso (titulo) VALUES ('Aritmética');
+INSERT INTO curso (titulo) VALUES ('Geometría');
+INSERT INTO curso (titulo) VALUES ('Álgebra');
+INSERT INTO curso (titulo) VALUES ('R.M.');
+INSERT INTO curso (titulo) VALUES ('Trigonometría');
+INSERT INTO curso (titulo) VALUES ('Física');
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Números', id
+FROM grado
+WHERE nivel_academico_id = 2;
+-- Aritmética
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Aritmética', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado IN ('Cuarto grado', 'Quinto grado');
+
+-- Geometría
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Geometría', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado IN ('Cuarto grado', 'Quinto grado');
+
+-- Álgebra
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Álgebra', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado IN ('Cuarto grado', 'Quinto grado');
+
+-- R.M.
+INSERT INTO curso (titulo, grado_id)
+SELECT 'R.M.', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado IN ('Cuarto grado', 'Quinto grado');
+-- Aritmética
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Aritmética', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado = 'Sexto grado';
+
+-- Geometría
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Geometría', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado = 'Sexto grado';
+
+-- Álgebra
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Álgebra', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado = 'Sexto grado';
+
+-- R.M.
+INSERT INTO curso (titulo, grado_id)
+SELECT 'R.M.', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado = 'Sexto grado';
+
+-- Trigonometría
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Trigonometría', id
+FROM grado
+WHERE nivel_academico_id = 3
+  AND nombre_grado = 'Sexto grado';
+-- Aritmética
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Aritmética', id
+FROM grado
+WHERE nivel_academico_id = 4;
+
+-- Geometría
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Geometría', id
+FROM grado
+WHERE nivel_academico_id = 4;
+
+-- Álgebra
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Álgebra', id
+FROM grado
+WHERE nivel_academico_id = 4;
+
+-- R.M.
+INSERT INTO curso (titulo, grado_id)
+SELECT 'R.M.', id
+FROM grado
+WHERE nivel_academico_id = 4;
+
+-- Trigonometría
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Trigonometría', id
+FROM grado
+WHERE nivel_academico_id = 4;
+
+-- Física
+INSERT INTO curso (titulo, grado_id)
+SELECT 'Física', id
+FROM grado
+WHERE nivel_academico_id = 4;
+
